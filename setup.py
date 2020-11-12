202545982491caae4e5a2ee528df85c0cf2b5a61
prashanthtr <prashanth.thattai@gmail.com>
2020-11-12 15:28:40 +0800

    paramManager with setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='paramManager',
+    version='0.1dev',
+    packages=['paramManager',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
