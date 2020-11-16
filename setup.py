8031b7512505c64804da4e5f0ea76e521f4d9079
prashanthtr <prashanth.thattai@gmail.com>
2020-11-16 16:27:28 +0800

    removed pckage directory

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(
     version='0.1dev',
     py_modules=['foo'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
-    package_dir={'': 'paramManager'}
+    long_description=open('README.txt').read()
 )
