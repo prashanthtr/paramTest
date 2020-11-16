9ef25979853c20c9198c7bc8ddc1264181a786c8
prashanthtr <prashanth.thattai@gmail.com>
2020-11-16 16:13:14 +0800

    changed package dir to root

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(
     py_modules=['foo'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
+    package_dir={'': './'}
 )
