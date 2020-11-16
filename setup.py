19ccc318be682c818bc67d22b19cdda25df9220d
prashanthtr <prashanth.thattai@gmail.com>
2020-11-16 16:17:46 +0800

    package dir to root setup is single .

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     py_modules=['foo'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
-    package_dir={'': './'}
+    package_dir={'': '.'}
 )
